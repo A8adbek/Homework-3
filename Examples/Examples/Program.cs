0a4ed43761ca7ada6a2dea8ad62741e47ed85daa
Asadbek Murodaliyev <murodaliyevasadbek2001@gmail.com>
2023-03-30 17:21:23 +0500

    All-integer-numbers

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Int32 num = 0;
+            String numbers = String.Empty;
+            String numbers1 = String.Empty;
+            Int32 counter = 1;
+            while (counter <= 100)
+            {
+                Console.Write("Number " + counter + " : ");
+                numbers = Console.ReadLine();
+                if (numbers == "q" || numbers == "Q")
+                {
+                    goto a;
+                }
+                num = Convert.ToInt32(numbers);
+                if (num <= 100)
+                {
+                    numbers1 += num;
+                    numbers1 += ", ";
+                } 
+                counter++;
+            }
+            a:
+            Console.WriteLine("Reply : " + numbers1);
         }
     }
 }
