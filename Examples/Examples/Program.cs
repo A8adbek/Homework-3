50971c4c98287e8fe0256a5acfd8992fd52659f0
Asadbek Murodaliyev <murodaliyevasadbek2001@gmail.com>
2023-03-30 15:00:56 +0500

    Max-and-min-numbers

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Int32 num = 0;
+            Int32 max = 0;
+            Int32 min = 0;
+            Int32 counter = 1;
+            Console.WriteLine("| Enter a number");
+            while (counter <= 4)
+            {
+                Console.Write("| Number " + counter + " : ");
+                num = int.Parse(Console.ReadLine()); 
+                counter++;
+                if(max < num)
+                {
+                    max = num;
+                    if(counter == 2)
+                    {
+                        min = num;
+                    }
+                } else if(min > num)
+                {
+                    min = num;
+                }
+            }
+            Console.WriteLine("| max : " + max);
+            Console.WriteLine("| min : " + min);
         }
     }
 }
