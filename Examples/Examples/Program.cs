7b4e53da25a867165cd50b30e6d4d23fd28bd124
Asadbek Murodaliyev <murodaliyevasadbek2001@gmail.com>
2023-03-30 14:28:17 +0500

    Factorial-Number

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Int32 num = 0;
+            Int32 counter = 1;
+            Int32 Factorial = 1;
+            Console.Write("| Enter a number : ");
+            num = int.Parse(Console.ReadLine());
+            while (num >= counter)
+            {
+                Factorial *= counter;
+                counter++;
+            }
+            Console.Write("| " + num + " is a factorial number : " + Factorial);
         }
     }
 }
